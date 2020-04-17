ecab0a1524738f006bb5a43f6770e039c494bbf1
Adam Friedman <adam@blok.party>
2020-04-17 16:07:36 -0700

    Styling changes

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -26,7 +26,7 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
           datasets: [
             {
               label: 'People',
-              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              backgroundColor: ['#6B5B95', '#009B77', '#DD4124'],
               data: [confirmed.value, recovered.value, deaths.value],
             },
           ],
